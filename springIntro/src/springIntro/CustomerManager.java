b00dfacfaf2ed84148a32d79d24c74f25715d02e
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-12-04 14:44:12 +0300

    IoC and Dependency Injection

---
+++ springIntro/src/springIntro/CustomerManager.java
@@ -0,0 +1,18 @@
+package springIntro;
+
+public class CustomerManager {
+	
+	private ICustomerDal customerDal;
+	
+	//generate constructor using fields
+	//CustomerManager parametre olarak bir ICustomerDal istiyor ve onun add()'ini çalýþtýrýyor
+	public CustomerManager(ICustomerDal customerDal) {
+		this.customerDal = customerDal;
+	}
+
+	public void add() {
+		//iþ kurallarý
+		customerDal.add();
+		// iþ kurallarindan geçiyorsa git ve customerDal'da ki db eklemeyi çaðýr.
+	}
+}
