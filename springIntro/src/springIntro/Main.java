c719df35f195ba7cbdd6f62228e9165c15d6fd93
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-12-09 20:50:14 +0300

    Constructor Arg

--- springIntro/src/springIntro/Main.java
+++ springIntro/src/springIntro/Main.java
@@ -10,9 +10,15 @@ public class Main {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		
+		ICustomerService customerService = context.getBean("service", ICustomerService.class);
+		
 		//CustomerManager manager = new CustomerManager(new CustomerDal());
 		CustomerManager manager = new CustomerManager(context.getBean("database", ICustomerDal.class));
 		manager.add();												//ICustomerDal tipinde database nesnesini istiyoruz
-	}																//ve database nesnesine baktigimizda, bize MySqlCustomerDal verecek yani onu newleyip verecek (arkaplanda bizim yerimize yapacak)
-
+																	//ve database nesnesine baktigimizda, bize MySqlCustomerDal verecek yani onu newleyip verecek (arkaplanda bizim yerimize yapacak)
+	
+		customerService.add();
+	
+	
+	}
 }
