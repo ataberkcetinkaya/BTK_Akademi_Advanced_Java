4f8f34bf54c2cadfb204e18a3f3b17bce54ad8da
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-12-10 18:31:31 +0300

    text value injection

--- springIntro/src/springIntro/CustomerDal.java
+++ springIntro/src/springIntro/CustomerDal.java
@@ -1,7 +1,20 @@
 package springIntro;
 
 public class CustomerDal implements ICustomerDal {
+	
+	String connectionString;
+	
+	public String getConnectionString() {
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+
+
 	public void add() {
+		System.out.println("Connection String: " + this.connectionString);
 		System.out.println("Saved to Oracle.");
 	}
 }
