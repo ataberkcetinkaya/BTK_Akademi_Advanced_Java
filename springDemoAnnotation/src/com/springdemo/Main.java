eb555014ef7588e0134276bfdd3504adddccbb28
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-12-11 20:01:58 +0300

    springDemoAnnotation

---
+++ springDemoAnnotation/src/com/springdemo/Main.java
@@ -0,0 +1,20 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//applicationContext.xml'i okuyacagimiz söyledik.
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		
+		ICustomerDal customerDal = context.getBean("service", ICustomerDal.class);
+		
+		
+		customerDal.add();
+	
+	
+	}
+}
