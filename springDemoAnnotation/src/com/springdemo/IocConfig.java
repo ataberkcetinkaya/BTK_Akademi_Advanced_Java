45cc5d9e58df4fba6bb5e0496cc16b0d3496c43d
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-12-11 21:25:24 +0300

    Bean for class config

--- springDemoAnnotation/src/com/springdemo/IocConfig.java
+++ springDemoAnnotation/src/com/springdemo/IocConfig.java
@@ -1,10 +1,14 @@
 package com.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //konfigürasyon dosyasi olduðunu belirtir.
 @ComponentScan("com.springdemo") //"com.springdemo"'daki database için tüm class'lari arayacak.
 public class IocConfig {
-
+	@Bean
+	public ICustomerDal database() { //ICustomerDal çünkü msSql'de mySql'de onu implemente eder.
+		return new MySqlCustomerDal();
+	}
 }
