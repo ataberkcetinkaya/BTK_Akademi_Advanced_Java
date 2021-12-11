ae4d6c3b5aa4ec6cf1128e86ffe5a350cba9b393
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-12-11 21:34:37 +0300

    resolve connected classes

---
+++ springDemoAnnotation/src/com/springdemo/CustomerManager.java
@@ -0,0 +1,19 @@
+package com.springdemo;
+
+public class CustomerManager implements ICustomerService {
+	
+	private ICustomerDal customerDal;
+	
+	//generate constructor using fields
+	//CustomerManager parametre olarak bir ICustomerDal istiyor ve onun add()'ini çalýþtýrýyor
+	//constructor injection
+	public CustomerManager(ICustomerDal customerDal) { 
+		this.customerDal = customerDal;
+	}
+	
+	public void add() {
+		//iþ kurallarý
+		//customerDal.add();
+		// iþ kurallarindan geçiyorsa git ve customerDal'da ki db eklemeyi çaðýr.
+	}
+}
