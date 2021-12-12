e6af8a87371d22163be3a37f79be78348d94c8a5
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-12-12 11:20:16 +0300

    First Query to Database

---
+++ hibernateDemo/src/com/hibernatedemo/Main.java
@@ -0,0 +1,37 @@
+package com.hibernatedemo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration() //mantik olarak configuration diye bir class oluþuyor ve sessionfactory'e atiyor
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(City.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession(); //fabrikadan ürettiðimiz bu session'i kullanarak artik sorgu gönderebiliriz
+	
+		try {
+			session.beginTransaction();
+			
+			List<City> cities = session.createQuery("from City").getResultList();
+									//Select * from city ile ayni anlamdadir
+					
+			for(City city:cities) {
+				System.out.println(city.getName());
+			}
+			
+			session.getTransaction().commit();
+			
+		}	finally {
+			factory.close();
+		}
+		
+	}
+
+}
